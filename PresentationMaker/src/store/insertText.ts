30c23718597f438d0f94517e9f8d08d487bee806
raketa0 <72815676+raketa0@users.noreply.github.com>
2024-12-13 01:13:35 +0300

    Delete PresentationMaker directory

--- PresentationMaker/src/store/insertText.ts
+++
@@ -1,9 +0,0 @@
-import {Slide, Texts} from "../store/TypesPresentation"
-//вставить текст
-function insertText(slide: Slide, SlideElement: Texts): Slide {
-    return {
-      ...slide,
-      elements: [...slide.elements, SlideElement],
-  };
-  }
-  export{insertText}
