a8d1c0e7fe59888a740f3cbd09e4a37eda5fff3b
raketa0 <72815676+raketa0@users.noreply.github.com>
2025-02-04 21:16:29 +0300

    Delete src directory

--- src/store/redux/actionCreators.ts
+++
@@ -1,9 +0,0 @@
-import * as SlideActionCreators from './slideActionCreators'
-import * as SelectionActionCreators from './selectionActionCreators'
-import * as EditorActionCreators from './editorActionCreators'
-
-export default {
-    ...SlideActionCreators,
-    ...SelectionActionCreators,
-    ...EditorActionCreators,
-}
--- src/store/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import { editorReducer } from "./editorReducer";
-
-const store = createStore(editorReducer)
-
-export {
-    store
-}
